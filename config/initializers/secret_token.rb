5f45248f3dc3bbfa67fea8f0707bdd62f9061153
Rodrigo Estebanez <restebanez@mdsol.com>
2011-10-09 19:43:47 -0400

    rails new agi. using rails 3.1.1 and ruby-1.9.2-p290

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Agi::Application.config.secret_token = '1db50f776aca81d318077f2581f4af300e29840774cd6ab2f39f106d3498745dcfb653b0ef99fb1e901136328ae460e09220eab74f6fc48eede5ca1d5c45160f'
