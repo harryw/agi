29b4983ffb5d9d748deb6c3f9bcc296a5cc1648d
Rodrigo Estebanez <restebanez@mdsol.com>
2012-01-06 14:55:33 -0500

    Fixing bug, alias_method was creating an infinate loop i believe

--- lib/active_resource/extend/mauth_signer.rb
+++ lib/active_resource/extend/mauth_signer.rb
@@ -7,9 +7,7 @@ module ActiveResource #:nodoc:
          # delete request(:delete, path, build_request_headers(headers, :delete, self.site.merge(path)))
          # put request(:put, path, body.to_s, build_request_headers(headers, :put, self.site.merge(path)))
          # post request(:post, path, body.to_s, build_request_headers(headers, :post, self.site.merge(path)))
-         
-         alias_method :old_request, :request
-         
+                  
          def request(method, path, *arguments)
            mauth_settings= YAML.load_file(File.join(Rails.root, "config", "mauth_settings.yml"))[:mauth]
 
@@ -18,8 +16,18 @@ module ActiveResource #:nodoc:
            h = MAuth::Signer.new(mauth_settings[:private_key]).signed_headers(:app_uuid => mauth_settings[:app_uuid], :request_url => path, :post_data => post_data, :verb => method.upcase.to_s)
            arguments.last["Authorization"]=h["Authorization"]
            arguments.last["x-mws-time"]=h["x-mws-time"]
-
-           old_request(method, path, *arguments)
+           
+           # this is the original part
+           result = ActiveSupport::Notifications.instrument("request.active_resource") do |payload|
+             payload[:method]      = method
+             payload[:request_uri] = "#{site.scheme}://#{site.host}:#{site.port}#{path}"
+             payload[:result]      = http.send(method, path, *arguments)
+           end
+           handle_response(result)
+         rescue Timeout::Error => e
+           raise TimeoutError.new(e.message)
+         rescue OpenSSL::SSL::SSLError => e
+           raise SSLError.new(e.message)
          end
        end
      end
