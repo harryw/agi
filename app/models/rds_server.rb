2216faa87741f6759a8f6a3939ad6f1f771a7cae
Rodrigo Estebanez <restebanez@mdsol.com>
2012-01-25 10:53:55 -0500

    agifog_settings live now on a file

--- app/models/rds_server.rb
+++ app/models/rds_server.rb
@@ -1,7 +1,7 @@
 class RdsServer < ActiveResource::Base
-  include ActiveResource::Extend::MauthSigner
-  
-  self.site = "http://localhost:3000/api/v1/rds/"
+#  include ActiveResource::Extend::MauthSigner
+  agifog_settings = YAML.load_file(File.join(Rails.root, "config", "agifog_settings.yml"))[:agifog]
+  self.site = "http://#{agifog_settings[:hostname]}:#{agifog_settings[:port]}/api/v1/rds/"
   self.element_name = "server"
   alias_attribute :name, :id
   
