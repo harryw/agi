b647bfd065f5d1fc412f041e30b21b09fab4419c
Rodrigo Estebanez <restebanez@mdsol.com>
2012-01-25 14:12:01 -0500

    adding app_status

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   
-  before_filter :authenticate_user!
+  before_filter :authenticate_user!, :except => [:app_status]
+  
+  def app_status
+    render(:json => "OK", :status => 200)
+  end
+  
   
   protected
   
