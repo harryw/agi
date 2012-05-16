e0068cc5b13acbd4e482473c972eb88a56ecdc0d
Rodrigo Estebanez <restebanez@mdsol.com>
2012-05-16 18:17:06 +0200

    If the PIR doesn't exist or Agi doesn't have access it will show it in deployments/new disabling the checkbox. Quite a few of refactoring in the Deployment model

--- app/controllers/deployments_controller.rb
+++ app/controllers/deployments_controller.rb
@@ -34,22 +34,12 @@ class DeploymentsController < ApplicationController
   # GET /deployments/new.json
   def new
     @deployment = @app.deployments.new
-    flash[:warning]= if !@app.database_attached?
-      "WARNING: database not attached, please attach a database to this app before deploying"
-    elsif !@app.database_started
-      "WARNING: database hasn't been started, please start it before deploying"
-    elsif !@app.database_ready?
-      "WARNING: database is not ready yet, please wait until the rds instance is available"
-    end
+    flash[:warning] = db_status
     # If the rds was restored from a snapshot, a few fields have to be modify after becomes available: security_groups, password, size
     # if the user went previously to the /databases#show page the following had been run
     @database_client = @app.database_sync_agi_fields_to_rds unless @app.database_snapshot_id.blank?
     load_deployment_data
-    
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @deployment }
-    end
+    verify_medistrano_pir_exists
   end
 
 
@@ -83,6 +73,30 @@ class DeploymentsController < ApplicationController
   
     def load_deployment_data
       @deployment_data = @app.generate_deployment_data
+    end
+    
+    def db_status
+      if !@app.database_attached?
+        "WARNING: database not attached, please attach a database to this app before deploying"
+      elsif !@app.database_started
+        "WARNING: database hasn't been started, please start it before deploying"
+      elsif !@app.database_ready?
+        "WARNING: database is not ready yet, please wait until the rds instance is available"
+      end
+    end
+    
+    def verify_medistrano_pir_exists
+      if @deployment.app_ec2_sg_to_authorize.blank?
+        @enable_merge_iq_with_medistrano_pir_checkbox = false
+      else
+        begin
+          @deployment.get_medistrano_pir # it raise an exception if either the Medistrano PIR doesn't exist or the access is denied
+          @enable_merge_iq_with_medistrano_pir_checkbox = true
+        rescue => e
+          @enable_merge_iq_with_medistrano_pir_checkbox = false
+          @error_hint = e.message
+        end
+      end
     end
 
 end
--- app/models/deployment.rb
+++ app/models/deployment.rb
@@ -3,7 +3,7 @@ class Deployment < ActiveRecord::Base
     belongs_to :app
     belongs_to :user
     
-    delegate :chef_account_update_data_bag_item, :lb_dns, :dynect_cname_status, :dynect_cname_name, :to => :app, :prefix => true
+    delegate :chef_account_update_data_bag_item, :lb_dns, :dynect_cname_status, :dynect_cname_name, :ec2_sg_to_authorize, :to => :app, :prefix => true
         
     serialize :deployed_data
     
@@ -13,97 +13,53 @@ class Deployment < ActiveRecord::Base
     :deployed_data, :force_deploy, :final_result, :opscode_result, :opscode_log, :description, :user_id, :s3_url_iq, :deployed_time,
     :merge_iq_with_medistrano_pir
         
-    #### IQ generation
-    
-    def generate_iq_file
-      pdf = IqDeployment.new(self,deploying_time)
-      pdf.render
-    end
-        
-    def save_iq_file
-      pdf_file_content = merge_iq_with_medistrano_pir ? merge_pir_with_iq : generate_iq_file
-      
+    def get_medistrano_pir
       begin
-        s3.get_bucket(iq_bucket) rescue s3.put_bucket(iq_bucket)
-        s3.directories.get(iq_bucket).files.create(:key => s3_key_name, :body=> pdf_file_content)
-      rescue => e
-        error=""
-        if match = e.message.match(/<Code>(.*)<\/Code>[\s\\\w]*<Message>(.*)<\/Message>/m)
-          error="#{match[1].split('.').last} => #{match[2]}"
+        s3.get_bucket(medistrano_pir_bucket_name)
+        if medistrano_pir_handler = s3.directories.get(medistrano_pir_bucket_name).files.get(medistrano_pir_key_name)
+          @medistrano_pir = medistrano_pir_handler.body
         else
-          error=e.message
+          raise "doesn't exist. Go to Medistrano and generate the PIR "
         end
-        Rails.logger.info "S3 FAILURE - #{e.message}"
-        self.errors.add(:remote_connection_to_s3, "it failed to save the IQ file in S3, #{error}")
-        return false
+      rescue => e          
+        raise "#{medistrano_pir_bucket_name}/#{medistrano_pir_key_name} #{try_to_parse_excon_aws_error(e)}"
       end
+      @medistrano_pir                    
     end
+
+    private
     
-    
-    def s3
-      @s3 ||= Fog::Storage::AWS.new(:aws_access_key_id => AppConfig["amazon_s3"]["access_key_id"], 
-                                    :aws_secret_access_key => AppConfig["amazon_s3"]["secret_access_key"])
-    end
-    
-    def s3_key_name
-      iq_folder = AppConfig["amazon_s3"]["bucket_name"].split('/')[1..-1].join('/')
-      @s3_key_name ||= "#{iq_folder}/#{app.name}/#{app.name}-#{deploying_time.to_s(:number)}.pdf".gsub(/^\//,"")
-    end
-    
-    def iq_bucket
-      AppConfig["amazon_s3"]["bucket_name"].split('/').first
-    end
-    
-    def deploying_time
-      @deploying_time ||= Time.now
-    end
-    
-    def gen_s3_url_iq
-      "https://#{iq_bucket}.s3.amazonaws.com/#{s3_key_name}"
-    end
-    
-    ######################
-    
-    
-    def set_initial_status
-      self.started_at = Time.now
-      self.final_result = 'Pending'
-      self.description = app.name if description.blank?
-      self.app_name = app.name
-    end
-    
-    def save_deployed_data
-      self.git_repo = app.project.repository
-      self.git_commit = app.git_revision
-      self.deployed_data = merged_configuration
-      self.s3_url_iq = gen_s3_url_iq
-      self.deployed_time = deploying_time
-    end
-   
-    def update_databag
-      begin
-        app_chef_account_update_data_bag_item(merged_configuration)
-        self.final_result = 'Success'
-        self.opscode_log= 'OK'
-      rescue 
-        self.opscode_log= $!.message 
-        self.final_result = 'Failed'  
+      def set_initial_status
+        self.started_at = Time.now
+        self.final_result = 'Pending'
+        self.description = app.name if description.blank?
+        self.app_name = app.name
       end
-      self.completed_at = Time.now  # self is required for assignments
-    end
-    
-    def configuration
-      attributes.symbolize_keys.extract!(:force_deploy,:send_email,:task,:run_migrations,
-                                                  :migration_command,:deployment_timestamp).merge(:deploy_by => self.try(:user).try(:email), :deployed_at => deploying_time)
-    end
-    
-    def merged_configuration
-      conf = app.generate_deployment_data
-      conf[:main].merge!(configuration)
-      conf
-    end
-    
-#    private
+      
+      def save_deployed_data
+        self.git_repo = app.project.repository
+        self.git_commit = app.git_revision
+        self.deployed_data = merged_configuration
+        self.s3_url_iq = gen_s3_url_iq
+        self.deployed_time = deploying_time
+      end
+      
+      def save_iq_file
+        pdf_file_raw = merge_iq_with_medistrano_pir ? merge_pir_with_iq : generate_iq_file
+        upload_to_s3(pdf_file_raw)
+      end
+      
+      def update_databag
+        begin
+          app_chef_account_update_data_bag_item(merged_configuration)
+          self.final_result = 'Success'
+          self.opscode_log= 'OK'
+        rescue 
+          self.opscode_log= $!.message 
+          self.final_result = 'Failed'  
+        end
+        self.completed_at = Time.now  # self is required for assignments
+      end
       
       def cname_load_balancer
         if app_lb_dns and app_dynect_cname_name
@@ -133,6 +89,76 @@ class Deployment < ActiveRecord::Base
           end
         end
       end
+    
+
+    
+      def generate_iq_file
+        pdf = IqDeployment.new(self,deploying_time)
+        pdf.render
+      end
+      
+      ####################################################################################################################################
+      # S3 
+      ####################################################################################################################################
+          
+      def gen_s3_url_iq
+        "https://#{iq_bucket}.s3.amazonaws.com/#{s3_key_name}"
+      end
+                  
+      def upload_to_s3(pdf_file_content)
+        begin
+          s3.get_bucket(iq_bucket) rescue s3.put_bucket(iq_bucket)
+          s3.directories.get(iq_bucket).files.create(:key => s3_key_name, :body=> pdf_file_content)
+        rescue => e
+          raise("it failed to save the IQ file in S3, #{try_to_parse_excon_aws_error(e)}")
+        end
+      end
+      
+      def s3
+        @s3 ||= Fog::Storage::AWS.new(:aws_access_key_id => AppConfig["amazon_s3"]["access_key_id"], 
+                                      :aws_secret_access_key => AppConfig["amazon_s3"]["secret_access_key"])
+      end
+      
+      def s3_key_name
+        iq_folder = AppConfig["amazon_s3"]["bucket_name"].split('/')[1..-1].join('/')
+        @s3_key_name ||= "#{iq_folder}/#{app.name}/#{app.name}-#{deploying_time.to_s(:number)}.pdf".gsub(/^\//,"")
+      end
+      
+      def iq_bucket
+        AppConfig["amazon_s3"]["bucket_name"].split('/').first
+      end
+      
+      
+      ####################################################################################################################################
+      # Deploy Configuration
+      ####################################################################################################################################
+      def deploying_time
+        @deploying_time ||= Time.now
+      end
+      
+      
+      def configuration
+        attributes.symbolize_keys.extract!(:force_deploy,:send_email,:task,:run_migrations,
+                                                    :migration_command,:deployment_timestamp).merge(:deploy_by => self.try(:user).try(:email), :deployed_at => deploying_time)
+      end
+      
+      def merged_configuration
+        conf = app.generate_deployment_data
+        conf[:main].merge!(configuration)
+        conf
+      end
+      
+      ####################################################################################################################################
+      # Util
+      ####################################################################################################################################
+    
+      def try_to_parse_excon_aws_error(rescue_error_msg)
+        if match = rescue_error_msg.message.match(/<Code>(.*)<\/Code>[\s\\\w]*<Message>(.*)<\/Message>/m)
+          "#{match[1].split('.').last} => #{match[2]}"
+        else
+          rescue_error_msg.message
+        end
+      end
       
       ####################################################################################################################################
       # Medistrano PIR
@@ -143,19 +169,9 @@ class Deployment < ActiveRecord::Base
       end
       
       def medistrano_pir_key_name
-        raise "ec2_sg_to_authorize isn't set, Agi can't determine the medistrano project and stage" if app.ec2_sg_to_authorize.blank?
-        medistrano_project, medistrano_stage, medistrano_cloud = app.ec2_sg_to_authorize.split(/-/)
+        raise "ec2_sg_to_authorize isn't set, Agi can't determine the medistrano project and stage" if app_ec2_sg_to_authorize.blank?
+        medistrano_project, medistrano_stage, medistrano_cloud = app_ec2_sg_to_authorize.split(/-/)
         "#{medistrano_project}/IQ/#{medistrano_project}-#{medistrano_stage}-PIR.pdf"
-      end
-      
-      def get_medistrano_pir
-        s3.get_bucket(medistrano_pir_bucket_name)
-        if medistrano_pir_handler = s3.directories.get(medistrano_pir_bucket_name).files.get(medistrano_pir_key_name)
-          @medistrano_pir = medistrano_pir_handler.body
-        else
-          raise "#{medistrano_pir_bucket_name}//#{medistrano_pir_key_name} doesn't exist. Go to Medistrano and generate the PIR "
-        end
-        @medistrano_pir                    
       end
       
       def save_medistrano_pir
