fb2cd3c78b82c946ea785ebd0e7b1398a9751711
Benton Roberts <benton@bentonroberts.com>
2012-05-31 10:15:30 -0400

    Create local DB user for cucumber tests

--- features/step_definitions/login_steps.rb
+++ features/step_definitions/login_steps.rb
@@ -1,6 +1,7 @@
 Given /^I am logged in$/ do
   Capybara.current_session.reset!
   CASClient::Frameworks::Rails::Filter.fake('user@test.com')
+  Factory.create(:user, :email => 'user@test.com')
 end
 
 Given /^I am not logged in$/ do
