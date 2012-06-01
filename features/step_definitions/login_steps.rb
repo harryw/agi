20f568082a10a836f67903f206e593791e8c0ad0
Benton Roberts <broberts@mdsol.com>
2012-06-01 13:21:52 -0400

    Adjust login feature step to fake correct credentials

--- features/step_definitions/login_steps.rb
+++ features/step_definitions/login_steps.rb
@@ -1,7 +1,7 @@
 Given /^I am logged in$/ do
   Capybara.current_session.reset!
-  CASClient::Frameworks::Rails::Filter.fake('user@test.com')
-  Factory.create(:user, :email => 'user@test.com')
+  CASClient::Frameworks::Rails::Filter.fake('user1')
+  FactoryGirl.create(:user, :email => 'user1@test.com', :username => 'user1')
 end
 
 Given /^I am not logged in$/ do
