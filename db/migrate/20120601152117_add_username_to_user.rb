017fcb83b9156c79d069dba8faee359c44008e73
Benton Roberts <broberts@mdsol.com>
2012-06-01 13:15:03 -0400

    Migrate legacy user data

--- db/migrate/20120601152117_add_username_to_user.rb
+++ db/migrate/20120601152117_add_username_to_user.rb
@@ -1,6 +1,15 @@
 class AddUsernameToUser < ActiveRecord::Migration
   def up
     add_column :users, :username, :string, { :after => :id, :null => false }
+    # Delete all users wihout mdsol addresses - they are not useful now
+    (User.all.select {|u| u.email !~ /@mdsol.com$/}).each {|u| u.destroy }
+    # Copy the username from the email
+    User.all.each do |user|
+      user.username = user.email.scan(/\A[^@]+/).first
+      # iMedidata insists on a 5-character minimum for username - fix exceptions
+      user.username = 'paulm' if user.email == 'pmin@mdsol.com' # Paul Min
+      user.save!
+    end
   end
 
   def down
